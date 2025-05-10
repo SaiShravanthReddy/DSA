87a4aa930270211f5d69545240607a2d55d3cc8b
SaiShravanthReddy <82753425+SaiShravanthReddy@users.noreply.github.com>
2025-05-10 11:44:13 +0530

    refactor taylor series

--- 5_recursion/10_taylor_series.cpp
+++ 5_recursion/10_taylor_series.cpp
@@ -2,16 +2,23 @@
 #include <stdlib.h>
 #include <iostream>
 
-int e(int x, int n) {
-    static int p = 1, f = 1; // static variables to store power and factorial
-    if (n == 0) return 1; // base case: e^0 = 1
+double e(int x, int n) {
+    static double p = 1, f = 1; // static variables to store power and factorial
+    double r;
+
+    if (n == 0) {
+        return 1; // base case: e^0 = 1
+    }
+    
+    r = e(x, n - 1);
     p = p * x; // calculate power of x
     f = f * n; // calculate factorial of n
-    return e(x, n - 1) + (double)p / f; // recursive call and add current term
+
+    return  r + (p / f); // recursive call and add current term
 }
 
 int main() {
-    int x = 1, n = 10; // example values for x and n
+    int x = 10, n = 10; // example values for x and n
     printf("e^%d = %f\n", x, e(x, n)); // print the result of e^x using Taylor series
     return 0;
 }
